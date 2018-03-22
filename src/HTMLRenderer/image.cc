7ea11ddd23d1a5af95a8c3f8c828a56f600691f2
Роман Нестерук <romanarasta@gmail.com>
2018-03-22 12:41:49 +0200

    update(image.cc)

--- src/HTMLRenderer/image.cc
+++ src/HTMLRenderer/image.cc
@@ -16,52 +16,36 @@ void HTMLRenderer::drawImage(GfxState * state, Object * ref, Stream * str, int w
 {
     tracer.draw_image(state);
 
-    #return OutputDev::drawImage(state,ref,str,width,height,colorMap,interpolate,maskColors,inlineImg);
+    return OutputDev::drawImage(state,ref,str,width,height,colorMap,interpolate,maskColors,inlineImg);
 
-if 1
+#if 1
     if(maskColors)
         return;
+    const char * filename = str_fmt("%s/f%llx%s", param.dest_dir.c_str(), image_count++, ".jpg");
+    FILE * f = fopen(filename, "wb");
+    if(!f)
+        throw string("Cannot open file for background image " ) + filename;
 
-    rgb8_image_t img(width, height);
-    auto imgview = view(img);
-    auto loc = imgview.xy_at(0,0);
+    std::unique_ptr<ImgWriter> writer;
+    writer = std::unique_ptr<ImgWriter>(new JpegWriter);
+    if(!writer->init(f, width, height, param.h_dpi, param.v_dpi))
+        throw "Cannot initialize image writer";
 
     ImageStream * img_stream = new ImageStream(str, width, colorMap->getNumPixelComps(), colorMap->getBits());
     img_stream->reset();
-
     for(int i = 0; i < height; ++i)
     {
         auto p = img_stream->getLine();
-        for(int j = 0; j < width; ++j)
-        {
-            GfxRGB rgb;
-            colorMap->getRGB(p, &rgb);
-
-            *loc = rgb8_pixel_t(colToByte(rgb.r), colToByte(rgb.g), colToByte(rgb.b));
-
-            p += colorMap->getNumPixelComps();
-
-            ++ loc.x();
-        }
-
-        loc = imgview.xy_at(0, i+1);
+        writer->writeRow(&p);
     }
 
-    png_write_view((format("i%|1$x|.png")%image_count).str(), imgview);
-    
-    img_stream->close();
-    delete img_stream;
+    if(!writer->close())
+    {
+        throw "Cannot finish background image";
+    }
 
-    close_line();
-
-    double ctm[6];
-    memcpy(ctm, state->getCTM(), sizeof(ctm));
-    ctm[4] = ctm[5] = 0.0;
-    html_fout << format("<img class=\"i t%2%\" style=\"left:%3%px;bottom:%4%px;width:%5%px;height:%6%px;\" src=\"i%|1$x|.png\" />") % image_count % install_transform_matrix(ctm) % state->getCurX() % state->getCurY() % width % height << endl;
-
-
-    ++ image_count;
-endif
+    fclose(f);
+#endif
 }
 
 void HTMLRenderer::drawSoftMaskedImage(GfxState *state, Object *ref, Stream *str,
